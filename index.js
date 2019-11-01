93be515ee8eacc1d8253c3fea597121533e6286c
Abstracted <antoine.luc@gmail.com>
2019-11-01 09:50:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -63,7 +63,11 @@ app.use(morgan('common'));
 // link to public static file ( documentation.html)
 app.use(express.static('public'));
 
-app.use('/client', express.static(path.join(__dirname, 'client/dist')));
+// 
+
+app.get("/client/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 
 // app.get("/client/*", (req, res) => {
