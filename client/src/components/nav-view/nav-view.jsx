6079c81f6d45d6be19cf28c826f59d8b5695e04b
Abstracted <antoine.luc@gmail.com>
2019-11-01 10:58:46 +0100

    test

--- client/src/components/nav-view/nav-view.jsx
+++ client/src/components/nav-view/nav-view.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Nav from 'react-bootstrap/Nav'
@@ -32,7 +32,7 @@ export function NavView(props) {
     <Nav.Link href="/client">MyFlix</Nav.Link>
   </Nav.Item>
   <Nav.Item>
-  <Nav.Link href="client/my-profile" eventKey="link-1">My Profile</Nav.Link>
+  <Nav.Link href="my-profile" eventKey="link-1">My Profile</Nav.Link>
 </Nav.Item>
 <Nav.Item>
   <Nav.Link href="/client"  eventKey="link-2"  onClick={handleDisconnect}> Disconnect</Nav.Link>
