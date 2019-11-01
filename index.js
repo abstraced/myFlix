491e747797b8329627b891c720fefef54f5c6acc
Abstracted <antoine.luc@gmail.com>
2019-11-01 10:30:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 require('./passport');
 
 // CORS
-var allowedOrigins = ['http://myflixdb.herokuapp.com','*'];
+var allowedOrigins = ['http://myflixdb.herokuapp.com','*','http://localhost:1234'];
 
 app.use(cors({
   origin: function(origin, callback){
